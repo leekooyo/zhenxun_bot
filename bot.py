7d1ed1428b9f73001ba2a705e8476ed875e64af9
leekooyo <95023320+leekooyo@users.noreply.github.com>
2025-03-22 18:32:15 +0800

    修改

--- bot.py
+++ bot.py
@@ -1,28 +1,31 @@
 import nonebot
-
+nonebot.init()
 # from nonebot.adapters.discord import Adapter as DiscordAdapter
 # from nonebot.adapters.dodo import Adapter as DoDoAdapter
 # from nonebot.adapters.kaiheila import Adapter as KaiheilaAdapter
 from nonebot.adapters.onebot.v11 import Adapter as OneBotV11Adapter
-
-nonebot.init()
-
-
-driver = nonebot.get_driver()
-driver.register_adapter(OneBotV11Adapter)
-# driver.register_adapter(KaiheilaAdapter)
-# driver.register_adapter(DoDoAdapter)
-# driver.register_adapter(DiscordAdapter)
-
 from zhenxun.services.db_context import disconnect, init
 
+# 初始化 NoneBot
+# 获取驱动
+driver = nonebot.get_driver()
+
+# 注册适配器
+driver.register_adapter(OneBotV11Adapter)
+
+# 启动和关闭时的操作
 driver.on_startup(init)
 driver.on_shutdown(disconnect)
 
-# nonebot.load_builtin_plugins("echo")
+# 加载 nonebot_plugin_apscheduler
+nonebot.load_plugin("nonebot_plugin_localstore")
+nonebot.load_plugin("nonebot_plugin_orm")
+nonebot.load_plugin("nonebot_plugin_session_orm")
+nonebot.load_plugin("nonebot_plugin_userinfo")
+
+# 加载其他插件
 nonebot.load_plugins("zhenxun/builtin_plugins")
 nonebot.load_plugins("zhenxun/plugins")
-
 
 if __name__ == "__main__":
     nonebot.run()
